27c31b5a51b9babb4842f15f5a1a34223c0244c0
Hicham Hajaj <hicham.hajaj@edu.uah.es>
2019-10-03 19:42:56 +0200

    factorial_sol.sh

---
+++ factorial_sol.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+echo -n "introduce un numero, por favor: "
+read counter
+factorial=1
+while [ $counter -gt 0 ];do
+	factorial=$(( $factorial * $counter ))
+	counter=$(( $counter - 1 ))
+
+
+done
+echo $factorial
