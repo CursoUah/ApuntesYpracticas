7dd0f46630487f3419eba3698fb3ec1806b4d287
msaez20 <56126452+msaez20@users.noreply.github.com>
2019-10-03 19:56:20 +0200

    Update factorial.sh

--- factorial.sh
+++ factorial.sh
@@ -2,7 +2,7 @@
 let resultado=1
 let contador=$1
 while [ $contador -gt 0 ];do
-	resultado=$(($resultado*$contador))
-	contador=$(($contador-1))
+	let resultado=$resultado*$contador
+	let contador=$contador-1
 done
 echo "resultado" $resultado
